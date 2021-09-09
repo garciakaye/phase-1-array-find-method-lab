9fd9bfd3402bd988d5ffdc90485dab98c42a7c8a
kaye garcia <kayemgarcia@gmail.com>
2021-09-09 11:53:26 -0500

    commit tree 2259f98fd31128bf7e83aba1564e3d60f516654b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const record = [
+    {year: '2018', result: 'L'},
+    {year: '2017', result: 'W'},
+    {year: '2017', result: 'N/A'}
+]
+
+// Write a function called superbowlWin()
+
+function superbowlWin(array){
+    const found = array.find(element => element.result === 'W');
+     return !!found ? found.year :  undefined
+}
 
